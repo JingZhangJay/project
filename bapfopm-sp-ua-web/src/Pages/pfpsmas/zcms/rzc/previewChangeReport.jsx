3545573d207a7690f2e13921b3852812a3ea2bf7
zhangjing <JingZhangJay@163.com>
2019-04-29 01:47:41 +0800

    变更明细报表样式调整

--- bapfopm-sp-ua-web/src/Pages/pfpsmas/zcms/rzc/previewChangeReport.jsx
+++ bapfopm-sp-ua-web/src/Pages/pfpsmas/zcms/rzc/previewChangeReport.jsx
@@ -18,21 +18,27 @@ export default class PreviewChangeReport extends React.Component {
         };
     }
 
-    changeVisible(type){
+    changeVisible(type) {
         this.setState({
             visible: type
         })
     }
 
-    componentWillMount(){
-        let {zoningCode} = this.state;
+    handleGoBack() {
+        window.history.back(-1); 
+        console.log(window.location);
+        console.log(document.getElementById('iframe').contentWindow.location)
+    }
+
+    componentWillMount() {
+        let { zoningCode } = this.state;
         let src = `http://10.1.92.19:9099/ureport/preview?_u=file:countZoning.ureport.xml&zoningCode=${zoningCode}`
         this.setState({
             iframeSrc: src
         })
     }
 
-    componentDidMount(){
+    componentDidMount() {
         // console.log(document.getElementById('iframe').contentDocument);
 
         // console.log(document.getElementById('iframe').contentWindow.document.getElementById('_ureport_table'));
@@ -49,7 +55,7 @@ export default class PreviewChangeReport extends React.Component {
                         start={'right'}
                         onScrollbarScrollTimeout={100}>
 
-                        <div className="previewChangeReport-container-top">
+                        <div className="previewChangeReport-container-top margin-top-10">
                             <Row>
                                 <Col span={12}>
                                     <span onClick={this.changeVisible.bind(this, "map")}>地图</span>
@@ -61,21 +67,25 @@ export default class PreviewChangeReport extends React.Component {
                         </div>
 
                         <div className="previewChangeReport-container-bottom">
-
                             {
-                                this.state.visible == "map" ? <GeoDemo></GeoDemo> : <iframe id="iframe" name='iframe' 
-                                    src={this.state.iframeSrc}
-                                    frameborder="0" 
-                                    style="filter:chroma(color=#ff0000)"
-                                    // scrolling="no" 
-                                    width="100%" 
-                                    height="500px" 
-                                    frameborder="0" 
-                                    allowTransparency="true">
-                                </iframe>
-                            }   
+                                this.state.visible == "map" ? <GeoDemo></GeoDemo> : 
+                                <div className="previewChangeReport-container-content">
+                                    <iframe id="iframe" name='iframe'
+                                        src={this.state.iframeSrc}
+                                        frameborder="0"
+                                        style="filter:chroma(color=#ff0000)"
+                                        // scrolling="no" 
+                                        width="100%"
+                                        height="500px"
+                                        frameborder="0"
+                                        allowTransparency="true">
+                                    </iframe>
+
+                                    <Button size="large" onClick={this.handleGoBack.bind(this)} className="previewChangeReport-container-button">返回</Button>
+                                </div>  
+                            }
                         </div>
-                    </FreeScrollBar>    
+                    </FreeScrollBar>
                 </div>
             </div>
         )
